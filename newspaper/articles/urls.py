cafd7af6562f20599cfd4d281f66e94ee95c529e
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-07-22 09:05:34 +0300

    Finish CRUD Operations on Article app

--- newspaper/articles/urls.py
+++ newspaper/articles/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name="home"),
+    path("article/<int:pk>", views.ArticleDetails, name="article_details"),
+    path("add_article", views.AddArticle, name="add_article"),
+    path("edit_article/<int:pk>", views.EditArticle, name="edit_article"),
+    path("delete_article/<int:pk>", views.DeleteArticle, name="delete_article"),
 ]
