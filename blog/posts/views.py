a7424e8eb07c915af0315f793a32b59ca0e0932e
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-07-26 09:30:15 +0300

    Add basic Home page

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post, Category
+
 
 # Create your views here.
+def Home(request):
+    categories = Category.objects.all()
+    context = {"categories": categories}
+    return render(request, "blog/home.html", context)
