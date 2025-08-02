8bf1c9f382939dfcb68fee8e4aa15f8e3da8522f
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-02 10:50:21 +0300

    Add Home page

--- planner/plans/views.py
+++ planner/plans/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Plan, Task
 
 # Create your views here.
+
+
+def Home(request):
+    user = request.user
+    plans = user.plans.all()
+    context = {"plans": plans}
+    return render(request, "plans/home.html", context)
