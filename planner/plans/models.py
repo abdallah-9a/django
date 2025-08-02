7f7d391ee4ca8483a0ec19652c9f49db79b4eca2
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-02 10:38:37 +0300

    Edit some mistakes

--- planner/plans/models.py
+++ planner/plans/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
 
 
 class Plan(models.Model):
     name = models.CharField(max_length=100)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="plans",default=None)
 
     def __str__(self):
         return self.name
