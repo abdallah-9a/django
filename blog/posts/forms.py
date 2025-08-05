795d0e1f650f897ac4ec23860c61c1f3cccb13fc
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-05 11:47:59 +0300

    Add form to force user input email while signup

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -1,13 +1,37 @@
-from django import forms 
+from django import forms
 from .models import Post, Comment, ForbiddenWords
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content',]
+        fields = [
+            "content",
+        ]
+
 
 class ForbiddensForm(forms.ModelForm):
     class Meta:
         model = ForbiddenWords
-        fields =["word",]
+        fields = [
+            "word",
+        ]
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True, help_text="Required. Enter a valid email address."
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
