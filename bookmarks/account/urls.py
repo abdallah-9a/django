4a3abd82638f7bb347873b1ff28abd1a251836d7
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-08 15:07:59 +0300

    Add urls for change password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # path("login", views.Login, name="login"), previous url
     path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
-    path("home",views.Home,name='home'),
+    path("password-change", auth_views.PasswordChangeView.as_view(),name = "password_change"),
+    path("password-change/done", auth_views.PasswordChangeDoneView.as_view(),name = "password_change_done"),
+    path("home",views.Home, name='home'),
 ]
