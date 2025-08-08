eb13492f7a4d9423d5cb82cd80f92b9fbf04dbda
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-08 15:18:02 +0300

    Add urls for Password Reset

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,9 +4,17 @@ from django.contrib.auth import views as auth_views  # Take fully control
 
 urlpatterns = [
     # path("login", views.Login, name="login"), previous url
+    path("home",views.Home, name='home'),
+    # Login\Logout urls
     path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
+    # Change Password urls
     path("password-change", auth_views.PasswordChangeView.as_view(),name = "password_change"),
     path("password-change/done", auth_views.PasswordChangeDoneView.as_view(),name = "password_change_done"),
-    path("home",views.Home, name='home'),
+    # Reset Password urls
+    path("password-reset", auth_views.PasswordResetView.as_view(), name= "password_reset"),
+    path("password-reset/done", auth_views.PasswordResetDoneView.as_view(), name = "password_reset_done"),
+    path("password-reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name = "password_reset_confirm"),
+    path("password-reset/complete", auth_views.PasswordResetCompleteView.as_view(), name= "password_reset_complete"),
+    
 ]
