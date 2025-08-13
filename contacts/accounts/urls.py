6749139520679abd2ee8161898285551d4ec7227
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-13 09:02:18 +0300

    Add Change password option

--- contacts/accounts/urls.py
+++ contacts/accounts/urls.py
@@ -3,4 +3,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("login/",auth_views.LoginView.as_view(),name='login'),
     path("logout/",auth_views.LogoutView.as_view(),name="logout"),
+    path("password-change/",auth_views.PasswordChangeView.as_view(), name="change_password"),
+    path("password-change/done/",auth_views.PasswordChangeDoneView.as_view(), name="change_password_done"),
 ]
