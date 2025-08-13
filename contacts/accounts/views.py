8620ed1ac8d6375eef271f06ab511743c16aaced
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-13 09:59:33 +0300

    Add SignUp

--- contacts/accounts/views.py
+++ contacts/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignUpForm
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class SignUp(CreateView):
+    model = User
+    form_class = SignUpForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
