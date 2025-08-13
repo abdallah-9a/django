15d7f4998149e2eb2915412409c77583ec9aff0b
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-13 14:12:30 +0300

    Raise error if duplicate email

--- contacts/accounts/forms.py
+++ contacts/accounts/forms.py
@@ -6,4 +6,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
-        
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("Email Already Exists")
+        return data
+    
