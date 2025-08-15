1ffa53eaafc7a9aebaac560b19e2828a64d2c0ee
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-15 15:57:38 +0300

    fix: correct password change redirect by updating URL names and template link

--- contacts/accounts/urls.py
+++ contacts/accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
-    path("login/",auth_views.LoginView.as_view(),name='login'),
-    path("logout/",auth_views.LogoutView.as_view(),name="logout"),
-    path("password-change/",auth_views.PasswordChangeView.as_view(), name="change_password"),
-    path("password-change/done/",auth_views.PasswordChangeDoneView.as_view(), name="change_password_done"),
-    path("signup",views.SignUp.as_view(), name="signup"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("signup", views.SignUp.as_view(), name="signup"),
 ]
