9e5876ca9dbeaf0e04caa1e9d8fa5498abf14db6
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-15 15:21:45 +0300

    Added AddContact view

--- contacts/contacts/views.py
+++ contacts/contacts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Contact
+from django.views.generic import CreateView, UpdateView, DeleteView
 # Create your views here.
+
+def Home(request):
+    if request.user.is_authenticated:
+        contacts = request.user.contacts.all()
+        context = {"contacts":contacts}
+        return render(request,"contacts/home.html",context)
+    return redirect("login")
+
+
+class AddContact(CreateView):
+    model = Contact
+    fields = ["username", 'email', 'phone']
+    template_name = "contacts/add_contact.html"
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
