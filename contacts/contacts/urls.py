e92f198699fa069ba1eb55250806115f26cf5934
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-16 15:28:58 +0300

    Refactor Home View to CBV with search and update urls and template

--- contacts/contacts/urls.py
+++ contacts/contacts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Home, name = "home"),
+    # path("", views.Home, name = "home"),
+    path("",views.Home.as_view(), name='home'),
     path("add",views.AddContact.as_view(),name="add_contact"),
     path("edit/<int:pk>",views.EditContact.as_view(),name="edit_contact"),
     path("delete/<int:pk>",views.DeleteContact.as_view(),name="delete_contact"),
--- contacts/contacts/views.py
+++ contacts/contacts/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render,redirect
 from .models import Contact
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from django.urls import reverse_lazy
+from django.db.models import Q
 # Create your views here.
 
-def Home(request):
-    if request.user.is_authenticated:
-        contacts = request.user.contacts.all()
-        context = {"contacts":contacts}
-        return render(request,"contacts/home.html",context)
-    return redirect("login")
+# def Home(request):
+#     if request.user.is_authenticated:
+#         contacts = request.user.contacts.all()
+#         context = {"contacts":contacts}
+#         return render(request,"contacts/home.html",context)
+#     return redirect("login")
 
+class Home(ListView):
+    model = Contact
+    template_name = "contacts/home.html"
+    context_object_name = "contacts"
+    
+    def get_queryset(self):
+        queryset = Contact.objects.filter(user = self.request.user)
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(Q(username__icontains=query)|
+                                       Q(email__icontains = query) |
+                                       Q(phone__icontains=query)   )
+        return queryset
 
 class AddContact(CreateView):
     model = Contact
