267578617709cc341873942a04c5eda4d7187c5f
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-18 18:20:04 +0300

    Add users app with custom Profile model

---
+++ freelancer_hub/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Profile(AbstractUser):
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    resume = models.FileField(upload_to="resumes/", blank=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("profile_details", kwargs={"pk": self.pk})
