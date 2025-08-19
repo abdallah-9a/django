8429a6b37249d942279762f83d5169873a8ac240
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-19 20:37:55 +0300

    Create Project model with initial fields

--- freelancer_hub/projects/models.py
+++ freelancer_hub/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from users.models import Profile
+from django.urls import reverse
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    budget = models.DecimalField(max_digits= 5,decimal_places=2)
+    created_by = models.ForeignKey(Profile,related_name="projects", on_delete=models.CASCADE)
+    deadline = models.DateField()
+    status = models.BooleanField(default=True) # True for open, False for closed
+    
+    def get_absolute_url(self):
+        return reverse("project_detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        return self.title
