eff16313a0293304b167019e96e08aaa05d4f2bc
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-19 20:44:35 +0300

    Implement Project list page (home)

--- freelancer_hub/projects/views.py
+++ freelancer_hub/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Project
 
 # Create your views here.
+
+class ProjectsList(ListView):
+    model = Project
+    context_object_name = "Projects"
+    template_name = "projects/home.html"
+
+
