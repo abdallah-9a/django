8adc8b5dad6c336a9b156bb5c9eddd862fafcc84
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-20 12:01:49 +0300

    Implement project delete functionality

--- freelancer_hub/projects/urls.py
+++ freelancer_hub/projects/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ProjectsList.as_view(), name="home"),
     path("project-detail/<int:pk>/", views.ProjectDetail.as_view(),name="project_detail"),
     path("edit/<int:pk>/",views.ProjectEdit.as_view(),name="edit_project"),
+    path("delete/<int:pk>/",views.ProjectDelete.as_view(),name="delete_project"),
     
 ]
--- freelancer_hub/projects/views.py
+++ freelancer_hub/projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, UpdateView,DeleteView
 from .models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 # Create your views here.
 
 class ProjectsList(ListView):
@@ -24,4 +25,11 @@ class ProjectEdit(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     
     def test_func(self):
         return self.request.user.id == self.get_object().created_by.id
+
+class ProjectDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy("home")
+    template_name = "projects/project_delete.html"
     
+    def test_func(self):
+        return self.request.user.id == self.get_object().created_by.id
