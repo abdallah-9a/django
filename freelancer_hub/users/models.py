45289f1abdb016ad87efe8bea2bd762fcc5e024f
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-08-24 09:19:19 +0300

    Add field last_seen in Profile model

--- freelancer_hub/users/models.py
+++ freelancer_hub/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
-
+from django.utils.timezone import now
 # Create your models here.
 
 
@@ -12,6 +12,7 @@ class Profile(AbstractUser):
     bio = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     location = models.CharField(max_length=120, blank=True, null=True)
+    last_seen = models.DateTimeField(default=now())
 
     def get_absolute_url(self):
         return reverse("profile_detail", kwargs={"pk": self.pk})
